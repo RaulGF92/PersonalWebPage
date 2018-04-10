222e813656b7207c4df368481341d51bd64ab450
raulgf92 <raulgf92@gmail.com>
2018-04-10 13:44:49 +0200

    make menu for mobile

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+function goTo(path) {
+  $("#nav").removeClass("openMenu");
+  window.location = "#" + path;
+}
+
+$("#menu").on("click", function(event) {
+  event.preventDefault();
+  $("#nav").toggleClass("openMenu");
+});
+
+$("#collapse").on("click", function(event) {
+    event.preventDefault();
+    $("#nav").removeClass("openMenu");
+  });
+  
+
+$("#menuProfile").on("click", function(event) {
+  event.preventDefault();
+  goTo("profile");
+});
+
+$("#menuStudies").on("click", function(event) {
+  event.preventDefault();
+  goTo("studies");
+});
+
+$("#menuContact").on("click", function(event) {
+  event.preventDefault();
+  goTo("contact");
+});
