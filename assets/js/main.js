e1cdc1c07721a5c6b6dee37d9d9cea78ec7ef933
raulgf92 <raulgf92@gmail.com>
2018-04-10 13:02:50 +0200

    solved some responsive fails

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-$(".list-group-item").on("click", function(event) {
-    $("#studiesFather").children().removeClass("active");
-    $(event.currentTarget).addClass("active");
-
-    $(".infoFill").css("display","none");
-    $("#"+event.currentTarget.id+"InfoFill").css("display","block");
-});
